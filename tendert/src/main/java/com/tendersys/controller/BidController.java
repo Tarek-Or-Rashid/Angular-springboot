2cc14c7ba13ed2b30b731d47773dedb4ab94be42
Tarek-Or-Rashid <tarekrashid386@gmail.com>
2025-09-06 00:12:57 +0600

    Update BidController.java

--- tendert/src/main/java/com/tendersys/controller/BidController.java
+++ tendert/src/main/java/com/tendersys/controller/BidController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/bids")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BidController {
 
     private final BidRepository bidRepository;
